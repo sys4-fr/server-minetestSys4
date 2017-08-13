2ab948e4e654dda2e87f015bec2096dd44fe7deb
sys4 <sys4@free.fr>
2017-08-13 15:56:05 +0200

    Clear craft of compressed cobble and make it with technic compressors

--- mods/sys4/init.lua
+++ mods/sys4/init.lua
@@ -1,4 +1,4 @@
--- Make xdecor and food bowl to be usable to drink water
+-- Make xdecor and food bowls to be usable to drink water
 if minetest.get_modpath("thirsty") then
 	if minetest.get_modpath("food") then
 		thirsty.config.register_bowl = false
@@ -20,4 +20,18 @@ if minetest.get_modpath("thirsty") then
 			 on_use = thirsty.on_use(nil)
 			})
 	end
+end
+
+-- If technic mod and moreblocks then clear compressed cobblestone craft and make it with technic compressor (LV or MV)
+if minetest.get_modpath("technic") and minetest.get_modpath("moreblocks") then
+   minetest.clear_craft(
+      { output = "moreblocks:cobble_compressed",
+	recipe = {
+	   {"default:cobble", "default:cobble", "default:cobble"},
+	   {"default:cobble", "default:cobble", "default:cobble"},
+	   {"default:cobble", "default:cobble", "default:cobble"}
+	}
+      })
+
+   technic.register_compressor_recipe({input = {"default:cobble 9"}, output = "moreblocks:cobble_compressed"})
 end
